ecc7df3d277115243929ba112d1d7ecaecba7123
Konrad Skrzypczyk <skrzypczykmail@gmail.com>
2019-11-29 12:47:25 +0100

    Configure SFML

---
+++ Paint/Source.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(250, 250), "Paint PK3");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+		{
+			sf::Vector2i position = sf::Mouse::getPosition();
+			std::cout << position.x << " " << position.y << std::endl;
+		}
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Paint C++ SFML
